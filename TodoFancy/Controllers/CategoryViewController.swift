809f86ee43f4bed65aa80af4e7d10e47e51eab1e
Faishal Murtadho <muhammad.faishal@myboost.id>
2019-09-02 06:50:11 +0700

    Create relation between category entity and item entity, init CategoryViewController.

---
+++ TodoFancy/Controllers/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryViewController.swift
+//  TodoFancy
+//
+//  Created by useradmin on 01/09/19.
+//  Copyright © 2019 Faishal Murtadho. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+}
